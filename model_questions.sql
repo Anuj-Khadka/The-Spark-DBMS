bc960934179b49b379e5a8a817141983ea12a485
Anuj-Khadka <anujkhadka008@gmail.com>
2024-05-01 23:52:31 -0400

    Update Model Question

--- model_questions.sql
+++ model_questions.sql
@@ -8,3 +8,17 @@ Left JOIN Classes cl ON s.stuID1 = cl.stuID1
 left join instructor i on cl.instructorID1 = i.instructorID1
 WHERE r.rating < 80
 ORDER BY r.rating ASC;
+
+
+
+
+-- 2. Find the course name and descriptions for each course, along with the description, ordered by the difficulty level in descending order.
+
+SELECT courseName, description, difficulty
+FROM Courses
+ORDER BY
+    CASE
+        WHEN difficulty = 'Advanced' THEN 1
+        WHEN difficulty = 'Intermediate' THEN 2
+        WHEN difficulty = 'Beginner' THEN 3
+    END DESC;
